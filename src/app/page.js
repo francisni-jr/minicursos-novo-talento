e3b3ac461a3ef7c57a770b04a76cf3af5b589a2d
francisnijr1.acc <francisnijr1.acc@gmail.com>
2024-06-02 17:49:04 +0000

    Disableing Eslint rules

--- src/app/page.js
+++ src/app/page.js
@@ -14,10 +14,6 @@ export default function Home() {
       <section class="mx-auto flex flex-col gap-10 w-full justify-center items-center">
    <div class="p-10 rounded-xl flex flex-col
    jstify-center items-start gap-5">
-     <div class="w-40" >
-     <img alt="example" className='' src="https://res.cloudinary.com/dtdz4se00/image/upload/v1713475087/Marca_PNG_tpcpob.png"
-            class="relative object-cover shrink-0 h-28 w-28 z-10 rounded-xl" />
-     </div>
      <h3 class="text-3xl text-s font-highlight">Curso de Vocação</h3>
      <p class="text-s" >Para conhecer o mundo, é necessário se
      conhecer primeiro. Um guia gratuito de autoconhecimento pessoal e
